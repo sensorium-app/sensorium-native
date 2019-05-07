e28b13be2f6c819f25bf0c577b0bd6b70d3dc098
Leonardo Gutiérrez Hernández <leo342135@gmail.com>
2019-05-06 19:16:11 -0500

    Navigation component installation

--- android/app/src/main/java/com/sensorium/sensoriumapp/MainApplication.java
+++ android/app/src/main/java/com/sensorium/sensoriumapp/MainApplication.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -45,6 +46,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.asList(
         new MainReactPackage(),
+            new RNGestureHandlerPackage(),
             new VectorIconsPackage(),
         new RNFirebasePackage(),
         // add/remove these packages as appropriate
