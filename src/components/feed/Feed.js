9b098749b2dcce6b3e1e3932577ed3751f4ddcd2
Leonardo Gutiérrez Hernández <leo342135@gmail.com>
2020-03-09 19:41:12 -0600

    Posts Feed scrolling

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
-import { View, Text, Button as NativeButton, StyleSheet } from 'react-native';
+import { 
+    View, Text, Button as NativeButton, StyleSheet, FlatList,
+    SafeAreaView, Dimensions,
+} from 'react-native';
 import {connect} from 'react-redux';
 import { mapDispatchToProps } from './../../actions';
 import { Icon } from 'react-native-elements';
-import { ScrollView } from 'react-native-gesture-handler';
 import Post from './Post';
+import Loader from './../loader/Loader';
+import Styles from './../Styles';
+
+const { height, width } = Dimensions.get('window');
 
 class Feed extends Component {
     static navigationOptions = {
@@ -15,6 +21,13 @@ class Feed extends Component {
         super(props);
 
         this.addPost = this.addPost.bind(this);
+        //this.renderFooter = this.renderFooter.bind(this);
+        //this.renderHeader = this.renderHeader.bind(this);
+        //this.retrieveMore = this.retrieveMore.bind(this);
+
+        this.state = {
+            loading: false,
+        }
     }
 
     componentWillMount() {
@@ -24,19 +37,47 @@ class Feed extends Component {
     }
 
     renderPosts(){
-        if(this.props.mainClusterPosts && this.props.mainClusterPosts.posts.length > 0){
-            return this.props.mainClusterPosts.posts
-            .map((post, i) => {
-                
-                return (
-                    <Post 
-                        key={i}
-                        postData={post}
-                    />
-                );
-            });
-        }
+        //if(this.props.mainClusterPosts && this.props.mainClusterPosts.posts.length > 0){
+            
+        //}
     }
+
+    retrieveMore = () => {
+        console.log('go for more...');
+        /*this.setState({
+            loading: true,
+        })*/
+    }
+
+    renderHeader = () => {
+        try {
+          return (
+            <Text style={styles.headerText}>Welcome!</Text>
+          )
+        }
+        catch (error) {
+          console.log(error);
+          return null;
+        }
+      };
+
+    renderFooter = () => {
+        try {
+          // Check If Loading
+          if (this.state.loading) {
+            return (
+              <Loader />
+            )
+          }
+          else {
+            return null;
+          }
+        }
+        catch (error) {
+          console.log(error);
+          return null;
+        }
+      };
 
     addPost(){
         this.props.navigation.navigate('AddPost');
@@ -44,25 +85,38 @@ class Feed extends Component {
 
     render() {
         return (
-            <View style={styles.container}>
-                <ScrollView>
-                    {
-                        this.props.mainClusterPosts ?
-                        this.renderPosts()
-                        : <Text>Loading...</Text>
-                    }
-                </ScrollView>
-                <View style={styles.floatingActionButton}>
-                    <Icon
-                        raised
-                        reverse
-                        name='plus'
-                        type='antdesign'
-                        color='purple'
-                        onPress={this.addPost}
+            (this.props.mainClusterPosts && this.props.mainClusterPosts.posts.length > 0) ?
+                <SafeAreaView style={styles.container}>
+                    <FlatList
+                        extraData={this.props.mainClusterPosts.posts}
+                        data={this.props.mainClusterPosts.posts}
+                        renderItem={({ item }) => (
+                            <Post 
+                                key={item.idRef}
+                                postData={item}
+                            />
+                        )}
+                        keyExtractor={(item, index) => String(index)}
+                        ListHeaderComponent={this.renderHeader}
+                        ListFooterComponent={this.renderFooter}
+                        onEndReached={this.retrieveMore}
+                        onEndReachedThreshold={0.2}
+                        refreshing={this.state.loading}
                     />
+                    <View style={styles.floatingActionButton}>
+                    <Icon
+                            raised
+                            reverse
+                            name='plus'
+                            type='antdesign'
+                            color='purple'
+                            onPress={this.addPost}
+                        />
+                    </View>
+                </SafeAreaView> :
+                <View style={Styles.container}>
+                    <Loader />
                 </View>
-            </View>
         );
     }
 }
@@ -78,8 +132,30 @@ const mapStateToProps = state => {
 export default connect(mapStateToProps, mapDispatchToProps)(Feed);
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         flex: 1,
+    },
+    headerText: {
+        fontFamily: 'System',
+        fontSize: 36,
+        fontWeight: '600',
+        color: '#000',
+        marginLeft: 12,
+        marginBottom: 12,
+    },
+    itemContainer: {
+        height: 80,
+        width: width,
+        borderWidth: .2,
+        borderColor: '#000',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        fontFamily: 'System',
+        fontSize: 16,
+        fontWeight: '400',
+        color: '#000',
     },
     floatingActionButton: {
         position: 'absolute',
